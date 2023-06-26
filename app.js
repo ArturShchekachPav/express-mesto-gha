66f20a65fd41a1e1b26a9d82a9c80c973125459e
ArturShchekachPav <arturshchekatch@yandex.ru>
2023-06-26 19:33:29 +1000

    feat: rise security level by helmet and rateLimit

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const routes = require('./routes');
 
-const {PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-})
-.then(() => {
-  console.log('connected to db');
 });
 
 const app = express();
 
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 15 * 60 * 1000,
+  message: 'Too many requests from this IP',
+});
+
+app.use(helmet());
+app.use(limiter);
 app.use((req, res, next) => {
   req.user = {
-    _id: '649814a3354441ec8ab3f73d' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '649814a3354441ec8ab3f73d', // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
@@ -24,6 +31,4 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-})
+app.listen(PORT);
