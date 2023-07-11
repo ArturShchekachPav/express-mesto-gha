f28648b659af79fc70367891daec3b9f819174e8
ArturShchekachPav <arturshchekatch@yandex.ru>
2023-07-11 13:13:27 +1000

    fix: fix joi validation

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,15 @@ const auth = require('../middlewares/auth');
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required(),
+    password: Joi.string().required()
   }),
 }), login);
 router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }).unknown(true),
 }), createUser);
 router.use('/users', auth, userRoutes);
